105cfd65d26fe0ba17830c541b90bc7f8cbf0fcc
Tejas Gulhane <tgulhane28@gmail.com>
2023-05-20 13:55:10 +0530

    VaccineBooking

---
+++ src/main/java/com/example/CovidVaccineBooking/CovidVaccineBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.CovidVaccineBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidVaccineBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidVaccineBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CovidVaccineBooking/CovidVaccineBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CovidVaccineBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidVaccineBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
